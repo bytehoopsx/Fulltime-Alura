aa0d735f823091d411c141623f753d8a7659140f
bytehoopsx <miguelscopimm@gmail.com>
2026-09-18 09:50:10 -0300

    Add files via upload

--- Projeto_AluGames/alugames/js/app.js
+++ Projeto_AluGames/alugames/js/app.js
@@ -2,14 +2,34 @@ function alterarStatus(id) {
     let gameClicado = document.getElementById(`game-${id}`);
     let imagem = gameClicado.querySelector('.dashboard__item__img');
     let botao = gameClicado.querySelector('.dashboard__item__button');
+    let nomeJogo = gameClicado.querySelector('.dashboard__item__name');
+    let jogosAlugados = 1 - 1;
 
-    if (imagem.classList.contains('dashboard__item__img--rented')) {
+    function contarEExibirJogosAlugados() {
+    console.log(`Total de jogos que você alugou: ${jogosAlugados}`);
+}
+
+if (imagem.classList.contains('dashboard__item__img--rented')) {
+        //confirmação para devolver o jogo
+        if (confirm(`Você tem certeza que deseja devolver o jogo ${nomeJogo.textContent}? Clique em "Ok" para Confirmar a devolução.`)) {
         imagem.classList.remove('dashboard__item__img--rented');
         botao.classList.remove('dashboard__item__button--return');
         botao.textContent = 'Alugar';
+        jogosAlugados++;
+        }
     } else {
         imagem.classList.add('dashboard__item__img--rented');
         botao.classList.add('dashboard__item__button--return');
         botao.textContent = 'Devolver';
+        jogosAlugados++;
+        
     }
+
+    contarEExibirJogosAlugados();
 }
+
+//inicia contagem de jogos já alugados :)
+    document.addEventListener('DOMContentLoaded', function() {
+    jogosAlugados = document.querySelectorAll('.dashboard__item__img--rented').length;
+    contarEExibirJogosAlugados();
+});
